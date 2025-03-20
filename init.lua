89f0be140be3647e99b4f0e58cd78bc8284d5236
Ryuta Oshikiri <ryushikiri@gmail.com>
2025-03-19 19:34:32 -0700

    adding go config

--- init.lua
+++ init.lua
@@ -10,7 +10,16 @@ vim.cmd([[source ~/.dot/vimrc]])
 -- empty setup using defaults
 require("nvim-tree").setup()
 require'nvim-web-devicons'.setup()
+
+require('dap-go').setup()
+require('goconfig')
 -- require'nvim-web-devicons'.get_icons()
 
 vim.opt.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
+
+--" Add (Neo)Vim's native statusline support.
+-- NOTE: Please see `:h coc-status` for integrations with external plugins that
+-- provide custom statusline: lightline.vim, vim-airline.
+-- vim.cmd([[set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}]])
+vim.opt.statusline = "%{coc#status()}%{get(b:,'coc_current_function','')}"
