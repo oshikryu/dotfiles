6c4b6dd2cab57eab4d38f91f72b2cafa8aef7ccc
Ryuta Oshikiri <ryushikiri@gmail.com>
2025-05-07 12:05:36 -0700

    vim git

--- init.lua
+++ init.lua
@@ -11,8 +11,6 @@ vim.cmd([[source ~/.dot/vimrc]])
 require("nvim-tree").setup()
 require'nvim-web-devicons'.setup()
 
-require('dap-go').setup()
-require('goconfig')
 -- require'nvim-web-devicons'.get_icons()
 
 vim.opt.background = "dark"
@@ -22,35 +20,9 @@ vim.cmd([[colorscheme gruvbox]])
 -- NOTE: Please see `:h coc-status` for integrations with external plugins that
 -- provide custom statusline: lightline.vim, vim-airline.
 -- vim.cmd([[set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}]])
-vim.opt.statusline = "%{coc#status()}%{get(b:,'coc_current_function','')}"
+-- vim.opt.statusline = "%{coc#status()}%{get(b:,'coc_current_function','')}"
 
-local dap = require('dap')
-
-dap.adapters.go = function(callback, _)
-  local port = 38697
-  local handle
-  handle = vim.loop.spawn("dlv", {
-    args = { "dap", "-l", "127.0.0.1:" .. port },
-    detached = true,
-  }, function(code)
-    handle:close()
-    print("Delve exited with code", code)
-  end)
-  vim.defer_fn(function()
-    callback({ type = "server", host = "127.0.0.1", port = port })
-  end, 100)
-end
-
-dap.configurations.go = {
-  {
-    type = "go",
-    name = "Run CLI with args",
-    request = "launch",
-    program = "/Users/ryuta.oshikiri/projects/develop-domino-connect-service/cmd/dom",
-    args = {
-      "connect",
-      "6801786a3420ca57e5b619c2",
-      "--domino-api-endpoint=https://your-api.domino.tech",
-    },
-  },
-}
+vim.keymap.set('i', '<F2>', function()
+  vim.o.paste = not vim.o.paste
+  print("Paste mode: " .. tostring(vim.o.paste))
+end, { noremap = true, silent = false })
