b9ba33154dace6f6898d733d0d36dea583d04832
Ryuta Oshikiri <ryuta.oshikiri@dominodatalab.com>
2025-06-16 08:45:18 -0700

    adding nvim-metals scala linting

--- init.lua
+++ init.lua
@@ -51,5 +51,48 @@ end, { noremap = true, silent = false })
 
 -- Enable jump to open file in vim-tree directory
 vim.keymap.set('n', '<leader>j', function()
-  require("nvim-tree.api").tree.toggle({ find_file = true, focus = true })
+  require("nvim-tree.api").tree.open({ find_file = true, focus = true })
 end, { desc = "Toggle NvimTree and find current file" })
+
+
+--  -----------------------------------
+-- nvim-metals setup
+--  -----------------------------------
+local metals_config = require("metals").bare_config()
+
+-- Enable status bar provider
+metals_config.init_options.statusBarProvider = "on"
+
+-- Optional: Enable debugging support
+metals_config.on_attach = function(client, bufnr)
+  require("metals").setup_dap()
+  -- Example keymaps (optional)
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
+    end
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap("K", vim.lsp.buf.hover, "[K] Hover")
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+end
+
+-- Auto start metals when opening scala/sbt files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt" },
+  callback = function()
+    require("metals").initialize_or_attach(metals_config)
+  end,
+})
+
+local dap = require("dap")
+vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP: Continue" })
+vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP: Step Over" })
+vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP: Step Into" })
+vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP: Step Out" })
+vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+vim.keymap.set("n", "<Leader>B", function()
+  dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "DAP: Set Conditional Breakpoint" })
